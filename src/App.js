ef327cd215302562e784a827f1ad227c63e882ed
Juan Martinez <jdmarti12@gmail.com>
2022-06-10 14:01:18 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch.js";
+import { TodoList } from "./TodoList.js";
+import { TodoItem } from "./TodoItem.js";
+import { CreateTodoButtom, CreateTodoButton } from "./CreateTodoButton.js";
+//import './App.css';
+
+const defaultTodos=[
+  {text:'Cortar cebolla', completed:true},
+  {text:'Tormar el curso de intro a react', completed:false},
+  {text:'Llorar con la llorona', completed:false}
+];
 
 function App() {
+  const[todos, setTodos] = react.useState(defaultTodos);
+  const [searchValue, setSearchValue] = react.useState('');
+  const completedTodos = todos.filter(todo => todo.completed == true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <react.Fragment>
+      <TodoCounter />    
+      <TodoSearch
+        searchValue = {searchValue}
+        setSearchValue ={setSearchValue}
+       />
+      <TodoList>
+        {todos.map(todo =>(<TodoItem key={todo.text} text={todo.text} completed={todo.completed} />))}
+      </TodoList>
+      <CreateTodoButton />      
+   </react.Fragment>
   );
 }
 
