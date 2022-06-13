4654aec543b223c4c8560dbc905d544cce77c7e4
Juan Martinez <jdmarti12@gmail.com>
2022-06-13 16:33:18 -0500

    creación de eliminación

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { TodoCounter } from "./TodoCounter";
 import { TodoSearch } from "./TodoSearch.js";
 import { TodoList } from "./TodoList.js";
 import { TodoItem } from "./TodoItem.js";
-import { CreateTodoButtom, CreateTodoButton } from "./CreateTodoButton.js";
+import { CreateTodoButton } from "./CreateTodoButton.js";
 //import './App.css';
 
 const defaultTodos=[
@@ -15,7 +15,7 @@ const defaultTodos=[
 function App() {
   const[todos, setTodos] = react.useState(defaultTodos);
   const [searchValue, setSearchValue] = react.useState('');
-  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const completedTodo = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
   let searchedTodos =[];
 
@@ -28,19 +28,35 @@ function App() {
       return todoText.includes(searchText);
     })
   }
+  const completeTodo = (text) =>{
+   const todoIndex = todos.findIndex(todo => todo.text === text);
+   const newTodos = [...todos];
+   newTodos[todoIndex] ={
+    text:todos[todoIndex].text,
+    completed: true
+   }
+   setTodos(newTodos);
+  }
+
+  const borrarTodo = (text) =>{
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex,1)
+    setTodos(newTodos);
+   }
 
   return (
    <react.Fragment>
       <TodoCounter
         total={totalTodos}
-        completed ={completedTodos}
+        completed ={completedTodo}
        />    
       <TodoSearch
         searchValue = {searchValue}
         setSearchValue ={setSearchValue}
        />
       <TodoList>
-        {searchedTodos.map(todo =>(<TodoItem key={todo.text} text={todo.text} completed={todo.completed} />))}
+        {searchedTodos.map(todo =>(<TodoItem key={todo.text} text={todo.text} completed={todo.completed} onDelete={()=>borrarTodo(todo.text)} onComplete={()=>completeTodo(todo.text)} />))}
       </TodoList>
       <CreateTodoButton />      
    </react.Fragment>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,17 +3,11 @@ import './TodoItem.css';
 
 function TodoItem(props) {
 
- const onComplete = () => {
-    alert('Ya completaste el todo' + props.text)
- }
- const onDelete = () => {
-  alert('Borraste el todo' + props.text)
-}
 
   return (
     <li className="TodoItem">
       <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-      onClick={onComplete}
+      onClick={props.onComplete}
       >
         √
       </span>
@@ -21,7 +15,7 @@ function TodoItem(props) {
         {props.text}
       </p>
       <span className="Icon Icon-delete"
-      onClick={onDelete}
+      onClick={props.onDelete}
       >
         X
       </span>
