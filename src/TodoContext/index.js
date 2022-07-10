711eb455fe98bdefc02d4a6d3d94dac96d574b37
Juan Martinez <jdmarti12@gmail.com>
2022-07-10 15:30:35 -0500

    cambios en el modal

--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -3,67 +3,72 @@ import { useLocarStorage } from './useLocarStorage';
 
 const TodoContext = React.createContext();
 
-function TodoProvider(props){
-    
-  const{
-    item:todos,
-    saveItem:saveTodos,
+function TodoProvider(props) {
+  const {
+    item: todos,
+    saveItem: saveTodos,
     loading,
-    error
+    error,
   } = useLocarStorage('TODOS_V1', []);
-  
   const [searchValue, setSearchValue] = React.useState('');
   const [openModal, setOpenModal] = React.useState(false);
+
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
-  let searchedTodos =[];
+
+  let searchedTodos = [];
 
   if (!searchValue.length >= 1) {
     searchedTodos = todos;
-  }else{
-    searchedTodos = todos.filter(todo =>{
+  } else {
+    searchedTodos = todos.filter(todo => {
       const todoText = todo.text.toLowerCase();
       const searchText = searchValue.toLowerCase();
       return todoText.includes(searchText);
-    })
+    });
   }
+  // Función para añadir un nuevo TODO
+  const addTodo = (text) => {
+    const newTodos = [...todos];
+    newTodos.push({
+      completed: false,
+      text,
+    });
+    saveTodos(newTodos);
+  };
 
-  const completeTodo = (text) =>{
-   const todoIndex = todos.findIndex(todo => todo.text === text);
-   const newTodos = [...todos];
-   newTodos[todoIndex] ={
-    text:todos[todoIndex].text,
-    completed: true
-   }
-   saveTodos(newTodos);
-  }
-
-  const deleteTodo = (text) =>{
+  const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
-    newTodos.splice(todoIndex,1)
+    newTodos[todoIndex].completed = true;
     saveTodos(newTodos);
-   }
-    return (
-        <TodoContext.Provider value={{
-            error,
-            loading,
-            totalTodos,
-            completedTodos,
-            searchValue,
-            setSearchValue,
-            searchedTodos,
-            completeTodo,
-            deleteTodo,
-            openModal,
-            setOpenModal
-        }}>
-            {props.children}
-        </TodoContext.Provider>
+  };
 
-    );
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+  
+  return (
+    <TodoContext.Provider value={{
+      loading,
+      error,
+      totalTodos,
+      completedTodos,
+      searchValue,
+      setSearchValue,
+      searchedTodos,
+      addTodo,
+      completeTodo,
+      deleteTodo,
+      openModal,
+      setOpenModal,
+    }}>
+      {props.children}
+    </TodoContext.Provider>
+  );
 }
 
-<TodoContext.Consumer></TodoContext.Consumer>
-
-export {TodoContext,TodoProvider}
+export { TodoContext, TodoProvider };
