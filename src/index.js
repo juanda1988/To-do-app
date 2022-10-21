8632282c69245b7d4e5b548b820f125cb3a00cd1
Juan Martinez <jdmarti12@gmail.com>
2022-10-21 11:16:32 -0500

    cambios en HOC

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import './index.css';
-import App from "./App";
+import "./index.css";
+//import App from "./App";
+
+function App(props) {
+  return (
+    <h1>
+      {props.saludo}, {props.nombre}
+    </h1>
+  );
+}
+
+function withwhatever() {
+  return function ComponenteDeVerdad(props) {
+    return <h2>Buenassss!</h2>;
+  };
+}
+
+const AppWithWharever = withwhatever(App);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<App saludo="Buenas" nombre="Nath" />);
